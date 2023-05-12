00608e9a20374a31000b267f138df0353cd2a7d8
Евгений <@DESKTOP-3L0U6U2>
2023-05-12 19:50:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,25 @@
+﻿// ConsoleApplication4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+public:
+	int re;
+	int im;
+	Complex(int perv, int pimv) {
+		this->re = perv;
+		this->im = pimv;
+	};
+
+private:
+
+};
+
+
+int main()
+{
+	Complex lok = new Complex(0, 7);
+}
